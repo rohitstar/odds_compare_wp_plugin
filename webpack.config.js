5a032e1ddf606b5f4e326c9a6b1ade99216b8d96
rohitstar <rohitchauhan140398@gmail.com>
2025-08-27 19:30:04 +0530

    Project structure created

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+module.exports = {
+    entry: {
+        'block-editor': './assets/js/block-editor.js',
+        'block-frontend': './assets/js/block-frontend.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].min.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+        ]
+    }
+};
